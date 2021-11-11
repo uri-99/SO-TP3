daecc7e680020f3fd74431706f507b042b0b2d4d
Uriel <umihura@itba.edu.ar>
2021-11-11 19:38:43 -0300

    basic structure

---
+++ client.c
@@ -0,0 +1,41 @@
+#include "includes/client.h"
+#include "socket.c"
+#include <sys/socket.h>
+#include <stdio.h>
+
+#define PORT 8080
+
+
+int main()
+{
+    //crear socket, despues connect
+    struct sockaddr_in server_address;
+    int sock;
+
+    if ((sock = socket(AF_INET, SOCK_STEAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);:
+
+
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+   
+    if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    //connection made
+    char* hello = "hi i just connected"
+    send(sock , hello , strlen(hello) , 0);
+    printf("Hello message sent\n");
+    return 0;
+}
+   
